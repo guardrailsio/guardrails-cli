fb8828e255c2c17a91bdc8939f2b1c94287d9382
wahyudibo <wahyudi.ibo.wibowo@gmail.com>
2022-09-07 17:12:03 +0700

    finish help template

---
+++ Makefile
@@ -0,0 +1,15 @@
+# create linux 64 bit binary.
+linux-bin:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./bin/guardrails-linux-amd64 .
+
+# create osx 64 bit binary.
+darwin-bin:
+	CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o ./bin/guardrails-amd64-darwin .
+
+# create windows 64 bit binary.
+windows-bin:
+	CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags="-w -s" -o ./bin/guardrails.exe .
+
+# install linux bin to ${GOPATH}/bin so it can be executed globally.
+install-linux-bin:
+	make linux-bin && chmod +x ./bin/guardrails-linux-amd64 && cp ./bin/guardrails-linux-amd64 ${GOPATH}/bin/guardrails
---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	tmpl := `
+  ____                     _ ____       _ _        ____ _     ___
+ / ___|_   _  __ _ _ __ __| |  _ \ __ _(_) |___   / ___| |   |_ _|
+| |  _| | | |/ _' | '__/ _' | |_) / _' | | / __| | |   | |    | |
+| |_| | |_| | (_| | | | (_| |  _ < (_| | | \__ \ | |___| |___ | |
+ \____|\__,_|\__,_|_|  \__,_|_| \_\__,_|_|_|___/  \____|_____|___|
+
+Usage: guardrails <command> [<args>]
+
+Commands:
+  scan [-t,--token=<token>][-p,--path=<path>][-f,--format=json,csv,sarif,pretty]
+       [-o,--output=<path>][-q,--quiet]
+  help
+
+scan scans a repository for vulnerabilities and output results
+  -t, --token  a valid Guardrails CLI token you can obtain from dashboard > settings
+  -p, --path   the path to the repository to scan, defaults to $PWD
+  -f, --format the output format for scan results, defaults to pretty
+  -o, --output if provided, will save the output to the specified file path
+  -q, --quiet  if provided, will only output scan results in --format and nothing else
+
+help displays this help menu
+`
+
+	rootCmd.SetHelpTemplate(tmpl)
+}
+
+func initConfig() {
+	viper.AutomaticEnv()
+}
